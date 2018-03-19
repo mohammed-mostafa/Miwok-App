25cde2a7ba9dc46a13e36fbbaecde2f3b6e991f4
mostafa <mohammed.mostufa@yahoo.com>
2018-03-19 21:07:39 +0200

    Use while loop to add TextViews to the screen for each word in the List View

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,6 +18,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -39,16 +41,19 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("eight") ;
         words.add("nine") ;
         words.add("ten") ;
-        //Modify the log statements
-        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
-        Log.v("NumbersActivity", "Word at index 1: " + words.get(1));
-        Log.v("NumbersActivity", "Word at index 2: " + words.get(2));
-        Log.v("NumbersActivity", "Word at index 3: " + words.get(3));
-        Log.v("NumbersActivity", "Word at index 4: " + words.get(4));
-        Log.v("NumbersActivity", "Word at index 5: " + words.get(5));
-        Log.v("NumbersActivity", "Word at index 6: " + words.get(6));
-        Log.v("NumbersActivity", "Word at index 7: " + words.get(7));
-        Log.v("NumbersActivity", "Word at index 8: " + words.get(8));
-        Log.v("NumbersActivity", "Word at index 9: " + words.get(9));
+        //find the root view
+        LinearLayout rootview = (LinearLayout)findViewById(R.id.rootview);
+        //create a variable for index
+        int index = 0;
+        //make a loop for all list
+        while (index<10){
+            //create new text view
+            TextView wordview = new TextView (this);
+            //set the text view to be  word at the current index
+            wordview.setText(words.get(index));
+            //add text view to the root view
+            rootview.addView(wordview);
+            index++;
+        }
     }
 }
