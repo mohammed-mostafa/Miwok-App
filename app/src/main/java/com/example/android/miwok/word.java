baf2012094c63b7496c8cbc3caa175045d8ac8ac
mostafa <mohammed.mostufa@yahoo.com>
2018-03-23 03:24:31 +0200

    Create a Custom Word Class

---
+++ app/src/main/java/com/example/android/miwok/word.java
@@ -0,0 +1,48 @@
+package com.example.android.miwok;
+
+/**
+ * Created by wellcome on 3/23/2018.
+ */
+
+public class word {
+
+
+    /**
+     * Default translation for the word
+     */
+    private String mDefaultTranslation;
+
+    /**
+     * Miwok translation for the word
+     */
+    private String mMiwokTranslation;
+
+    /**
+     * +     * Create a new Word object.
+     * +     *
+     * +     * @param defaultTranslation is the word in a language that the user is already familiar with
+     * +     *                           (such as English)
+     * +     * @param miwokTranslation is the word in the Miwok language
+     * +
+     */
+    public word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * +     * Get the default translation of the word.
+     * +
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * +     * Get the Miwok translation of the word.
+     * +
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
